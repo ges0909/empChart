36f7576105d920dc22c640f6b86f4bf79e762bf0
Gerrit Schrader <gerrit.schrader@infinit-services.de>
2018-01-19 17:48:48 +0100

    asset bug fix

--- assets.js
+++ assets.js
@@ -1,18 +1,17 @@
 const CSS = [
-    'jquery-ui-dist/jquery-ui.css',
-    'qunit/qunit.css'
+    'qunit/qunit/qunit.css'
 ];
+
 const JS = [
-    'flot-axislabels/jquery.flot.axislabels.js',
-    'flot.tooltip/js/jquery.flot.tooltip.js',
     'jquery/dist/jquery.js',
     'jquery-csv/src/jquery.csv.js',
     'jquery.flot/jquery.flot.js',
-    'jquery.flot/jquery.flot.resize.js',
     'jquery.flot/jquery.flot.time.js',
+    'jquery.flot/jquery.flot.resize.js',
+    'jquery.flot.tooltip/js/jquery.flot.tooltip.js',
     'lodash/lodash.js',
     'moment/moment.js',
-    'qunit/qunit.js'
+    'qunit/qunit/qunit.js'
 ];
 
 module.exports = [...JS, ...CSS];
