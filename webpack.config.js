5e33b40f8af821a62bbdf19fd91ed472c707dfe2
Gerrit Schrader <gerrit.schrader@infinit-services.de>
2018-01-24 17:54:58 +0100

    refactored

--- webpack.config.js
+++ webpack.config.js
@@ -8,31 +8,47 @@ const Assets = require('./assets');
 module.exports = {
   entry: {
     // use relative pathes only
-    story_1_1: './public_html/story_1_1.html',
-    story_1_4: './public_html/story_1_4.html',
-    story_1_5: './public_html/story_1_5.html',
-    story_1_6: './public_html/story_1_6.html',
-    story_1_6_1: './public_html/story_1_6_1.html',
-    story_2_1: './public_html/story_2_1.html',
-    story_2_2: './public_html/story_2_1.html'
+    chart: './src/js.chart.js',
+    story: './src/js/story.js'
+    // story_1_1: './public_html/story_1_1.html',
+    // story_1_4: './public_html/story_1_4.html',
+    // story_1_5: './public_html/story_1_5.html',
+    // story_1_6: './public_html/story_1_6.html',
+    // story_1_6_1: './public_html/story_1_6_1.html',
+    // story_2_1: './public_html/story_2_1.html',
+    // story_2_2: './public_html/story_2_1.html'
   },
   output: {
-    path: __dirname + '/public_html/',
+    path: __dirname + '/dist/',
     filename: '[name].bundle.js'
   },
   module: {
     rules: [
-      { test: /\.html$/, use: 'html-loader' }
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      }
     ]
   },
   plugins: [
     new CopyWebpackPlugin(
-            Assets.map(asset => {
-              return {
-                from: path.resolve(__dirname, `./node_modules/${asset}`),
-                to: path.resolve(__dirname, './public_html/ext')
-              };
-            })
-            )
+      Assets.map(asset => {
+        return {
+          from: path.resolve(__dirname, `./node_modules/${asset}`),
+          to: path.resolve(__dirname, './dist/ext')
+        };
+      })
+    )
   ]
 };
